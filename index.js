18396df1b788dad0ccb6a92d9f168f88c307c280
stkirk <shaanekirk@gmail.com>
2021-05-15 12:14:27 -0400

    completed Task 2 - Counter, wrote summation function that receives a number and uses a counter to return a summation of that number

--- index.js
+++ index.js
@@ -26,9 +26,15 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
+function summation(num) {
+  let counter = 0;
+  for (let i = 1; i < num + 1; i++) {
+    counter += i;
+  }
+  return counter;
 }
+console.log("summation", summation(4));
+console.log("summation", summation(5));
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
